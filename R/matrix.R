2e81d3aacd77a61f02617221423360db8f236fdf
Jaime Ashander <jashander@ucdavis.edu>
2012-04-25 11:18:18 -0700

    Initial commit. Includes basic matrix functions in R/matrix.R and some initial PVA tools in pva.R

---
+++ R/matrix.R
@@ -0,0 +1,87 @@
+
+#' Compute dominant eigenvalue
+#' @param A projection matrix for population model x(t+1) = A x(t)
+#' @details The dominant eigenvalue is the eigenvalue of A with the largest real part.
+#' @examples
+#' A = matrix(c(0,1,.5,0), nrow=2)
+#' domEig(A)
+#' @export
+domEig<-function(A){ 
+  lambda<-Re(eigen(A)$values[1]) # dominant eigenvalue
+  return(lambda)}
+
+
+#' Compute stable age distribution
+#' @param A projection matrix for population model x(t+1) = A x(t)
+#' @details The stable age distribution is the left eigenvector corresponding to
+#' the dominant eigenvalue of A.
+#' @examples
+#' A = matrix(c(0,1,.5,0), nrow=2)
+#' stabAge(A)
+#' @export
+stabAge<-function(A){
+  v<-Re(eigen(A)$vector[,1])
+  v<-v/sum(v)  # stable age distribution
+  return(v)}
+
+#' Compute reproductive value vector
+#' @param A projection matrix for population model x(t+1) = A x(t)
+#' @details The reproductive value of a stage/age class is the expected 
+#' future contribution to population growth of an individual in that class.
+#' Mathematically, this corresponds to the right eigenvector of the dominant
+#' eigenvalue of A.
+#' @examples
+#' A = matrix(c(0,1,.5,0), nrow=2)
+#' repVal(A)
+#' @export
+repVal<-function(A){
+  v<-stabAge(A)
+  w<-Re(eigen(t(A))$vector[,1])
+  w<-w/sum(v*w)  # reproductive values
+  return(w)
+}
+
+#' Compute a matrix of sensitivities
+#' @param A projection matrix for population model x(t+1) = A x(t)
+#' @details The sensitivity is defined as the change in dominant eigenvalue,
+#' i.e., the growth rate, with changes in an entry in matrix A.
+#' See Caswell 2001.
+#' @examples
+#' A = matrix(c(0,1,.5,0), nrow=2)
+#' Sens(A)
+#' @export
+Sens <- function(A){
+  v.A <- stabAge(A)
+  w.A <- repVal(A)
+  return(w.A%*%t(v.A)*sign(A))
+}
+
+#' Compute a matrix of sensitivities
+#' @param A projection matrix for population model x(t+1) = A x(t)
+#' @details The sensitivity is defined as the change in dominant eigenvalue,
+#' i.e., the growth rate, with changes in an entry in matrix A.
+#' See Caswell 2001.
+#' @examples
+#' A = matrix(c(0,1,.5,0), nrow=2)
+#' Elast(A)
+#' @export
+Elast <- function(A){
+  S.A <-  Sens(A)
+  lam <- domEig(A)
+  return(S.A*A/lam)
+}
+
+#' Flip a matrix so plotting with image makes visual sense 
+#' @param A projection matrix for population model x(t+1) = A x(t)
+#' @details R plotting function image can plot matrices, but default layout
+#' won't correspond to the way we're used to seeing matrices.
+#' This function transposes then reorders the columns. 
+#' See help for function image.
+#' @examples
+#' A = matrix(c(0,1,.5,0), nrow=2)
+#' Sens(A)
+#' image(mflip(A))
+#' @export
+mflip<-function(A){t(A)[,ncol(A):1]}
+
+
---
+++ README.markdown
@@ -0,0 +1,12 @@
+## Development instructions 
+
+Edit and add new functions in the files in the R/ directory. 
+For conceptually separable sets of functions, add a new file. 
+
+For best results use Hadley Wickham's `devtools`. Note you must also install `roxygen2`. 
+
+`install.packages('devtools')`
+`load(devtools)`
+
+For common tasks, etc, see the [devtools github page](https://github.com/hadley/devtools)
+
